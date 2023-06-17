aadae53ff8e46ad2fd2ccc6bce9e073478e6d4fe
RavenP <ravenp.dev@gmail.com>
2023-06-18 05:16:51 +0800

    row, flexible, and expanded widgets

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Flutter Basics
+
+#### Row
+
+- Shows children in a horizontal list
+- Cannot be scrolled
+- To align children use mainAxisAlignment and crossAxisAlignment
+
+#### Flexible
+
+- If you use multiple Flexible Widget in a row or column, you can tell them how they should relate to each other with the flex parameter
+- Will be created last (fixed size widgets will be created first and dynamic ones after, so they know how much space they have)
+
+#### Expanded
+
+- Special form of a Flexible widget (fit: FlexFit.tight)
+- Uses the available space given by flex
--- lib/main.dart
+++ lib/main.dart
@@ -22,30 +22,76 @@ class MyApp extends StatelessWidget {
           title: const Text('Flutter Basics'),
           centerTitle: true,
         ),
-        body: Container(),
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          color: Colors.white70,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Flexible(
+                    child: Text(
+                      "max steffen - Freelancer for flutter - living in germany - how are you?",
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Container(
+                    height: 20,
+                    width: 20,
+                    color: Colors.blue,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Container(
+                    height: 20,
+                    width: 20,
+                    color: Colors.green,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                ],
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.orange,
+                ),
+                child: const Text('Raven D. Paragas'),
+              ),
+              Container(
+                color: Colors.deepPurple,
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text('Hello World'),
+                      Text('Hi'),
+                      Text('There!'),
+                    ],
+                  ),
+                ),
+              ),
+              // Expanded(
+              //   child: Container(
+              //     color: Colors.red,
+              //   ),
+              // ),
+              const Text('After Expanded'),
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: const Icon(Icons.add),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                child: Text(
-                  'Drawer Header',
-                ),
-              ),
-              ListTile(
-                title: const Text('Item 1'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Item 2'),
-                onTap: () {},
-              )
-            ],
-          ),
         ),
       ),
     );
