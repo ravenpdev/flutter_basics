1609e30988021b9ac19577986ef4bb8d5cf3e4a8
RavenP <ravenp.dev@gmail.com>
2023-06-21 02:16:05 +0800

    what is BuildContext and layout builder widget

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Flutter Basics
+
+#### What is BuildContext
+
+- BuildContext is a way for your widget to know its location within the widget tree. It gives you a context about its location.
+- BuildContext provides location vertically but not horizontally
+
+#### Everything is a Widget (is a lie "partially")
+
+#### Element Tree
+
+Handles the lifecyle of the application
+
+_Each flutter widget has a corresponding element_
+
+**Purpose:**
+
+1. Hold the references to parent and child widgets
+2. Hold the state of the widget
+
+#### Render Tree
+
+In charge of display the UI
--- lib/main.dart
+++ lib/main.dart
@@ -22,30 +22,50 @@ class MyApp extends StatelessWidget {
           title: const Text('Flutter Basics'),
           centerTitle: true,
         ),
-        body: Container(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                child: Text(
-                  'Drawer Header',
+        body: Column(
+          children: [
+            Builder(builder: (context) {
+              return Container(
+                color: Colors.green,
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                child: const Center(
+                  child: Text('MediaQuery Inside Builder'),
                 ),
-              ),
-              ListTile(
-                title: const Text('Item 1'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Item 2'),
-                onTap: () {},
-              )
-            ],
-          ),
+              );
+            }),
+            const SizedBox(height: 20),
+            LayoutBuilder(builder: (context, constraints) {
+              return Container(
+                color: Colors.blue,
+                width: 200,
+                height: 200,
+                child: const Center(
+                  child: Text('MediaQuery Inside Builder'),
+                ),
+              );
+            }),
+            const SizedBox(height: 20),
+            Container(
+              width: 300,
+              height: 300,
+              color: Colors.yellow,
+              child: LayoutBuilder(builder: (context, constraint) {
+                // constraint contains data of the Container parent
+                // constraint.maxWidth is the width of the parent Container
+                return Center(
+                  child: Container(
+                    color: Colors.deepPurple,
+                    width: constraint.maxWidth / 2,
+                    height: constraint.maxHeight / 2,
+                    child: const Center(
+                      child: Text('Layout Builder'),
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ],
         ),
       ),
     );
