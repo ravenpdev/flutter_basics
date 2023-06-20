86fb87bdb6e65e5413dcb2fed684e9602983f296
RavenP <ravenp.dev@gmail.com>
2023-06-21 01:36:03 +0800

    media query

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print(MediaQuery.of(context).size.width);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
@@ -22,29 +23,121 @@ class MyApp extends StatelessWidget {
           title: const Text('Flutter Basics'),
           centerTitle: true,
         ),
-        body: Container(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                child: Text(
-                  'Drawer Header',
+        body: Container(
+          color: Colors.white,
+          // width: MediaQuery.of(context).size.width,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.lime,
+                  child: Row(
+                    children: [
+                      const Expanded(
+                          child: Text('My super long text is here! wazzup')),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        height: 50,
+                        width: 50,
+                        color: Colors.red,
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        height: 50,
+                        width: 50,
+                        color: Colors.blue,
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        height: 50,
+                        width: 50,
+                        color: Colors.orange,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              ListTile(
-                title: const Text('Item 1'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: const Text('Item 2'),
-                onTap: () {},
-              )
-            ],
+                const SizedBox(height: 20),
+                Container(
+                  color: Colors.teal,
+                  height: 100,
+                  width: 100,
+                  child: const Text('First'),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  color: Colors.grey,
+                  height: 100,
+                  width: 100,
+                  child: const Text('Second'),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  color: Colors.blue,
+                  height: 100,
+                  width: 100,
+                  child: const Text('Third'),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  color: Colors.orange,
+                  height: 100,
+                  width: 100,
+                  child: const Text('Fourth'),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  color: Colors.yellow,
+                  height: 100,
+                  width: 100,
+                  child: const Text('Fourth'),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  color: Colors.pink,
+                  height: 100,
+                  width: 100,
+                  child: const Text('Fifth'),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  color: Colors.red,
+                  height: 100,
+                  width: 100,
+                  child: const Text('Sixth'),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  color: Colors.black,
+                  height: 100,
+                  width: 100,
+                  child: const Text('Sixth'),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  color: Colors.deepPurple,
+                  height: 100,
+                  width: MediaQuery.of(context).size.width,
+                  alignment: Alignment.center,
+                  child: const Text('Media Query'),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  color: Colors.deepPurple,
+                  height: 100,
+                  width: MediaQuery.of(context).size.width / 2,
+                  alignment: Alignment.center,
+                  child: const Text('Media Query'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
