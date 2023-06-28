79f89c570b72347e3474fdcef98c73d57aa423a3
RavenP <ravenp.dev@gmail.com>
2023-06-29 00:30:50 +0800

    Basic navigation using BottomNavigatorBar

---
+++ lib/shared/root_bottom_navigation/root_bottom_navigation.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basics/about/about_screen.dart';
+import 'package:flutter_basics/home/home_screen.dart';
+import 'package:flutter_basics/profile/profile_screen.dart';
+
+class RootBottomNavigation extends StatefulWidget {
+  const RootBottomNavigation({super.key});
+
+  @override
+  State<RootBottomNavigation> createState() => _RootBottomNavigationState();
+}
+
+class _RootBottomNavigationState extends State<RootBottomNavigation> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Navigation')),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: const [
+          HomeScreen(),
+          ProfileScreen(),
+          AboutScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        currentIndex: _currentIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Users',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'About',
+          ),
+        ],
+      ),
+    );
+  }
+}
